47fc542caa7fbc61ec4c0ea365a64ff6c41ff5bf
Kabsa_Abdi <KabsaAbdi23@gmail.com>
2019-12-13 15:31:17 -0800

    new

---
+++ Person.py
@@ -0,0 +1,36 @@
+import random
+from Virus import Virus
+
+class Person:
+    ''' The simulation will contain people who will make up a population.'''
+
+    def __init__(self, is_vaccinated, infection=None):
+        ''' We start out with is_alive = True
+        All other values will be set by the simulation through the parameters when it instantiates each Person object.
+        '''
+        self.is_alive = True #boolean
+        self.is_vaccinated = is_vaccinated #boolean
+        self.infection = infection #virus object
+
+
+    def did_survive_infection(self):
+        ''' Generate a random number between 0.0 and 1.0 and compare to the virus's mortality_num.
+        If the random number is smaller, person dies from the disease. Set the person's is alive attribute to False
+        If Person survives, they become vaccinated and they have no infection (set the vaccinated attibute to True and the infection to None)
+        Return True if they survived the infection and False if they did not.
+        '''
+
+        # if infection is not None.
+        if self.infection is not None:
+        # Check random float against mortality_num
+            survival_chance = random.random()
+            if survival_chance <= self.infection.mortality_num:
+                self.is_alive = False
+                return False
+            else:
+                self.is_vaccinated = True
+                self.infection = None
+                return True
+                # Return None if there is no virus
+            #if self.infection = None:
+            #    return None
